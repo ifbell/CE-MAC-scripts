ebb52ed7b4cf68e3b6e6aa2cd97f4a1a705f6863
IFBell <31656082+ifbell@users.noreply.github.com>
2018-07-19 13:20:09 -0400

    Add files via upload

---
+++ clam_mcafee_uninstall.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+## postinstall
+
+#uninstall McAfee from the machine
+
+McAfeeuninstall() {
+/usr/local/McAfee/uninstall EPM
+#/usr/local/GSAfiles/uninstall.sh
+rm -fdvr /usr/local/GSAfiles/uninstall.sh
+rm -fdvr /Library/LaunchDaemons/com.mcafee.*
+rm -fdvr /Library/LaunchAgents/com.mcafee.*
+rm -fdvr /private/etc/cma.d 
+rm -fdvr /Applications/McAfee\ Endpoint\ Protection\ for\ Mac.app
+
+if [ ! -d /etc/cma.d/ ];then
+echo $(date) "We have found McAfee anti-virus files!" >> /var/log/GSAlog ; exit 1
+fi
+echo $(date) "Uninstalled McAfee anti-virus"  >> /var/log/GSAlog
+}
+#check for what antivirus exists
+if [ -d /Applications/ClamXAV.app ]; then
+/usr/sbin/installer -pkg /usr/local/clamXav/ClamXavEngineRemover.pkg -target /
+fi
+
+if [ -d /Applications/McAfee\ Endpoint\ Security\ for\ Mac.app ]; then
+McAfeeuninstall
+fi
+exit 0
+
+exit 0		## Success
+exit 1		## Failure
